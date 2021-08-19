59a0b83cbb44928f38fb7d8d725c9e2b6e4c6cbf
Robert I <robertorbbit@gmail.com>
2021-08-19 20:29:57 +0500

    Добавить проект Names

---
+++ Names/HeatmapTask.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+
+namespace Names
+{
+    internal static class HeatmapTask
+    {
+        public static HeatmapData GetBirthsPerDateHeatmap(NameData[] names)
+        {
+            var xLabels = new string[30];
+            var yLabels = new string[12];
+            var heatValues = new double[30, 12];
+            for (int y = 0; y < yLabels.Length; y++)
+            {
+                yLabels[y] = (y + 1).ToString();
+                for (int x = 0; x < xLabels.Length; x++)
+                {
+                    xLabels[x] = (x + 2).ToString();
+                    heatValues[x, y] = names.Count(n => n.BirthDate.Day == x + 2 && n.BirthDate.Month == y + 1);
+                }
+            }
+            return new HeatmapData(
+                "Пример карты интенсивностей",
+                heatValues, 
+                xLabels, 
+                yLabels);
+        }
+    }
+}
---
+++ Names/HistogramTask.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace Names
+{
+    internal static class HistogramTask
+    {
+        public static HistogramData GetBirthsPerDayHistogram(NameData[] names, string name)
+        {
+            names = names.Where(n => n.Name == name).ToArray();
+            var xLabels = new string[31];
+            var ages = new double[31];
+
+            for (int i = 0; i < xLabels.Length; i++)
+            {
+                xLabels[i] = (i + 1).ToString();
+                ages[i] = i !=0 ? names.Count(n => n.BirthDate.Day == i + 1) : 0;
+            }
+
+            return new HistogramData(
+                string.Format("Рождаемость людей с именем '{0}'", name), 
+                xLabels, 
+                ages);
+        }
+    }
+}
